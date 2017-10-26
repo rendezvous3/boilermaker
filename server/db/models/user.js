df46a01b936e124e5ff6bff989f7534045ac5d08
rendezvous3 <bojanj6m@gmail.com>
2017-10-26 01:21:54 -0400

    updated user model with crypto, salt, password

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto')
+const _ = require('lodash')
 const Sequelize = require('sequelize')
 const db = require('../db')
 
@@ -6,7 +8,39 @@ const User = db.define('user', {
     type: Sequelize.STRING,
     unique: true,
     allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+  },
+  salt: {
+    type: Sequelize.STRING,
   }
 })
 
 module.exports = User
+
+// instance methods
+User.prototype.correctPassword = function(candidatePwd){
+  return User.encryptPassword(candidatePwd, this.salt) === this.password
+}
+
+// class methods
+User.generateSalt = function () {
+  return crypto.randomBytes(16).toString('base64')
+}
+
+User.encryptPassword = function(plainText, salt){
+  return crypto.createHash('RSA-SHA256').update(plainText).update(salt).digest('hex')
+}
+
+const seaSaltAndPassword = user => {
+  if (user.changed('password')) {
+    user.salt = User.generateSalt()
+    user.password = User.encryptPassword(user.password, user.salt)
+  }
+}
+
+User.beforeCreate(setSaltAndPassword)
+User.beforeUpdate(setSaltAndPassword)
+
+
